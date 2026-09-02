0064e962617719b61d996fce4054b30cad06f9c9
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-09-02 17:45:46 +0300

    feat(layout): add demo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
 <div class="vsk-wrapper">
   <admin-layout :user="user" :menu="menu">
-    <muk-text type="muk-subheading" variant="primary">Content</muk-text>
+    <page-header title="Products" description="Manage your store products and inventory" :breadcrumbs="[{ label: 'Dashboard', to: '/'}, { label: 'Products'}]">
+      <template #actions>
+         <muk-icon-button  variant="secondary" size="md">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" ><path d="M480-480ZM202-65l-56-57 118-118h-90v-80h226v226h-80v-89L202-65Zm278-15v-80h240v-440H520v-200H240v400h-80v-400q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H480Z"/></svg>
+          <template #text> export</template>
+         </muk-icon-button>
+          <muk-icon-button variant="success"  size="md">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" ><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
+            <template #text>add product</template>
+          </muk-icon-button>
+      </template>
+    </page-header>
+    <muk-text>/* === Content === */</muk-text>
     <muk-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis autem dolorem rem?</muk-text>
   </admin-layout>
   </div>
@@ -12,9 +24,9 @@
 import { ref } from 'vue'
 
 /* COMPONENTS */
-import { MukText} from 'modular-ui-kit-vue';
+import { MukText, MukIconButton} from 'modular-ui-kit-vue';
 import AdminLayout from './lib/components/AdminLayout.vue';
-
+import PageHeader from './lib/components/PageHeader.vue';
 /* Types */
 import type { MenuItem, UserProfile } from '../src/lib/types/index.ts'
 

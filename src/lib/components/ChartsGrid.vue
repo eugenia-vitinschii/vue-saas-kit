b92c0d6b7b3dd5c251b6f906727c8d9597e4dd8f
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-09-15 17:29:18 +0300

    feat(charts-grid): add adaptive grid layout component for MukChart

---
+++ src/lib/components/ChartsGrid.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="vsk-charts-grid muk-chart__wrapper">
+   <template v-for="(item, index) in items" :key="item.id || index">
+      <muk-chart :="item"/>
+   </template>
+</div>
+</template>
+
+<script setup lang="ts">
+/* COMPONENTS  */
+import { MukChart } from 'modular-ui-kit-vue';
+/* CHART JS */
+import type { ChartData, ChartOptions, ChartType } from 'chart.js';
+
+export interface ChartItem{
+   id?: string | number
+   title?: string
+   loading?: boolean
+   emptyText?: string
+   height?: string
+   type?: ChartType
+   data: ChartData | null
+   options?: ChartOptions
+   [key: string]: any
+}
+
+defineProps<{
+   items: ChartItem[]
+}>()
+</script>

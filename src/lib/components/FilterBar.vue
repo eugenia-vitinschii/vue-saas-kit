118c7ce100c15dfeadbe04a35d5c8e6e0cfa4e9d
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-09-24 17:14:43 +0300

    feat(filter-bar): add slot-based FilterBar layout component

---
+++ src/lib/components/FilterBar.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="vsk-filter-bar">
+   <div class="vsk-filter-bar__filters">
+      <div v-if="$slots.search" class="vsk-filter-bar__search">
+         <slot name="search"></slot>
+      </div>
+      <div v-if="$slots.filters" class="vsk-filter-bar__group">
+         <slot name="filters"></slot>
+      </div>
+      <slot name="reset"></slot>
+   </div>
+      <div v-if="$slots.actions"  class="vsk-filter-bar__actions">
+         <slot name="actions"></slot>
+      </div>
+</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
